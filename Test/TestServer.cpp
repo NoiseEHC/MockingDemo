c58bf9d73e2666e6902187a91009cb20d3271a81
Andrew Rafas <andras.rafas@gmail.com>
2018-11-27 12:38:04 +1300

    Refactor into shared mock setup

--- Test/TestServer.cpp
+++ Test/TestServer.cpp
@@ -9,56 +9,50 @@ using namespace fakeit;
 
 TEST_CASE("Test the file handling")
 {
-    Mock<IFileHandler> mockHandler{};
-    When(Method(mockHandler,OpenOutput)).AlwaysDo([]() { return make_unique<ostringstream>(); });
-    Fake(Method(mockHandler,FinishOutput));
-    Fake(Dtor(mockHandler));
     Mock<ICommunicationThread> mockComm{};
     When(Method(mockComm,Start)).AlwaysDo([&](function<void(string)> callback) { ; });
     Fake(Method(mockComm,Stop));
     Fake(Dtor(mockComm));
-
-    Server sut{unique_ptr<IFileHandler>{&mockHandler.get()}, unique_ptr<ICommunicationThread>{&mockComm.get()}};
-    auto const processLine = FriendClass::GetProcessLine(sut);
-
-    processLine("1");
-    processLine("2");
-    processLine("3");
-    processLine("4");
-    processLine("5");
-
-    Verify(Method(mockHandler,OpenOutput)).Once();
-    Verify(Method(mockHandler,FinishOutput)).Once();
-    Verify(Method(mockComm,Start)).Once();
-}
-
-TEST_CASE("Stop saves remaining items")
-{
     Mock<IFileHandler> mockHandler{};
     When(Method(mockHandler,OpenOutput)).AlwaysDo([]() { return make_unique<ostringstream>(); });
-    string output;
-    When(Method(mockHandler,FinishOutput)).AlwaysDo([&](ostream& file)
+    Fake(Dtor(mockHandler));
+
+    SECTION("Test SaveLines")
     {
-        output = dynamic_cast<ostringstream&>(file).str();
-    });
-    Fake(Dtor(mockHandler));
-    Mock<ICommunicationThread> mockComm{};
-    When(Method(mockComm,Start)).AlwaysDo([&](function<void(string)> callback) { ; });
-    Fake(Method(mockComm,Stop));
-    Fake(Dtor(mockComm));
+        Fake(Method(mockHandler,FinishOutput));
 
-    Server sut{unique_ptr<IFileHandler>{&mockHandler.get()}, unique_ptr<ICommunicationThread>{&mockComm.get()}};
-    auto const processLine = FriendClass::GetProcessLine(sut);
+        Server sut{unique_ptr<IFileHandler>{&mockHandler.get()}, unique_ptr<ICommunicationThread>{&mockComm.get()}};
+        auto const processLine = FriendClass::GetProcessLine(sut);
 
-    processLine("1");
-    processLine("2");
-    processLine("3");
+        processLine("1");
+        processLine("2");
+        processLine("3");
+        processLine("4");
+        processLine("5");
+    }
 
-    sut.Stop();
+    SECTION("Stop saves remaining items")
+    {
+        string output;
+        When(Method(mockHandler,FinishOutput)).AlwaysDo([&](ostream& file)
+        {
+            output = dynamic_cast<ostringstream&>(file).str();
+        });
+
+        Server sut{unique_ptr<IFileHandler>{&mockHandler.get()}, unique_ptr<ICommunicationThread>{&mockComm.get()}};
+        auto const processLine = FriendClass::GetProcessLine(sut);
+
+        processLine("1");
+        processLine("2");
+        processLine("3");
+
+        sut.Stop();
+
+        Verify(Method(mockComm,Stop)).Once();
+        CHECK(count(begin(output), end(output), '\n') == 3);
+    }
 
     Verify(Method(mockHandler,OpenOutput)).Once();
     Verify(Method(mockHandler,FinishOutput)).Once();
     Verify(Method(mockComm,Start)).Once();
-    Verify(Method(mockComm,Stop)).Once();
-    CHECK(count(begin(output), end(output), '\n') == 3);
 }
